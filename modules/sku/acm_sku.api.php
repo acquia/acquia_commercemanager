99ff10a367051603d0bda6b29885bfd80c879d53
nikunj <me@nik4u.com>
2018-05-15 23:28:26 +0530

    Store commerce category status in Taxonomy term field.

--- modules/sku/acm_sku.api.php
+++ modules/sku/acm_sku.api.php
@@ -5,6 +5,9 @@
  * Hooks specific to the acm_sku module.
  */
 
+use Drupal\node\NodeInterface;
+use Drupal\taxonomy\TermInterface;
+
 /**
  * @addtogroup hooks
  * @{
@@ -22,6 +25,23 @@
  *   Array containing details provided by API.
  */
 function hook_acm_sku_product_node_alter(NodeInterface $node, array $product) {
+
+}
+
+/**
+ * Alter Taxonomy Term before it is saved during insert or update.
+ *
+ * Category data from API is passed here to allow other modules to read from
+ * the data provided by API and update Taxonomy Term accordingly.
+ *
+ * @param \Drupal\taxonomy\TermInterface $term
+ *   Taxonomy term to alter.
+ * @param array $category
+ *   Array containing details provided by API.
+ * @param mixed $parent
+ *   Parent Taxonomy term to if available.
+ */
+function hook_acm_sku_commerce_category_alter(TermInterface $term, array $category, $parent) {
 
 }
 
