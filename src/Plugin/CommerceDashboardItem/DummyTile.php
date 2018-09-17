efe87b429deaf9b87d9f75990a36a7e5a0dfdce2
Miro Michalicka <miroslav.michalicka@cheppers.com>
2018-09-17 21:12:54 +0200

    Move code from acm_diagnostics and add controller.

---
+++ src/Plugin/CommerceDashboardItem/DummyTile.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\acm\Plugin\CommerceDashboardItem;
+
+use Drupal\acm\CommerceDashboardItemBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class ConnectorAccessiblityRequirement.
+ *
+ * @CommerceDashboardItem(
+ *   id = "dummy_tile",
+ *   title = @Translation("Dummy Tile"),
+ *   weight = -1,
+ *   group = "tile",
+ * )
+ */
+class DummyTile extends CommerceDashboardItemBase {
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render() {
+    return [
+      '#theme' => "dashboard_item__" . $this->pluginDefinition['group'],
+      '#title' => $this->title(),
+      '#value' => 'Hello dashboard tile',
+    ];
+  }
+
+}
