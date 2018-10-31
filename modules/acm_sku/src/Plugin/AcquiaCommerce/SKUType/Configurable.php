bab29e6fa89e4f4d0427542b6225af2357038bb7
Rohit Joshi <joshi.rohit100@gmail.com>
2018-10-31 16:37:43 +0530

    Dispatch event in case not sku on cart submit.

--- modules/acm_sku/src/Plugin/AcquiaCommerce/SKUType/Configurable.php
+++ modules/acm_sku/src/Plugin/AcquiaCommerce/SKUType/Configurable.php
@@ -374,7 +374,13 @@ class Configurable extends SKUPluginBase {
       }
     }
     else {
-      drupal_set_message(t('The current selection does not appear to be valid.'));
+      $message = t('The current selection does not appear to be valid.');
+      drupal_set_message($message);
+      // Dispatch event so action can be taken.
+      $dispatcher = \Drupal::service('event_dispatcher');
+      $exception = new \Exception($message);
+      $event = new AddToCartErrorEvent($exception);
+      $dispatcher->dispatch(AddToCartErrorEvent::SUBMIT, $event);
     }
   }
 
