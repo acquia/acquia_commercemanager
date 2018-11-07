1a2442b4fd37f2ec1fda971e7d3a546631499d42
vbouchet <vincent.bouchet.31@gmail.com>
2018-11-07 12:28:22 +0100

    Provide utility methods.

--- modules/acm_sku/src/ProductInfoHelper.php
+++ modules/acm_sku/src/ProductInfoHelper.php
@@ -44,7 +44,7 @@ class ProductInfoHelper {
    * @return mixed
    *   Processed value.
    */
-  protected function getValue(SKUInterface $sku, string $field_code, string $context, $value) {
+  public function getValue(SKUInterface $sku, string $field_code, string $context, $value) {
     $event = new ProductInfoRequestedEvent($sku, $field_code, $context, $value);
     $this->eventDispatcher->dispatch(ProductInfoRequestedEvents::EVENT_NAME, $event);
     return $event->getValue();
@@ -64,6 +64,21 @@ class ProductInfoHelper {
   public function getTitle(SKUInterface $sku, string $context) {
     $default = $sku->label();
     return $this->getValue($sku, 'title', $context, $default);
+  }
+
+  /**
+   * Get media items for particular SKU.
+   *
+   * @param \Drupal\acm_sku\Entity\SKUInterface $sku
+   *   SKU Entity.
+   * @param string $context
+   *   Context to apply rules plp/pdp/basket.
+   *
+   * @return array
+   *   Processed media items.
+   */
+  public function getMedia(SKUInterface $sku, string $context) {
+    return $this->getValue($sku, 'media', $context, []);
   }
 
 }
